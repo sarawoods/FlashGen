f2a217b7347369b68c4a92e6eb26434ecd1682d5
RemingtonMaxwell <rsmaxwel@ucsc.edu>
2015-03-03 23:48:08 -0800

    Added shuffle button, worked on left scrolling

--- GUI.py
+++ GUI.py
@@ -8,6 +8,8 @@ import random
 
 from subprocess import check_output
 import json
+import wx.lib.buttons as buttons
+import wx.lib.agw.aquabutton as AB
 
 import  wx.lib.scrolledpanel as scrolled
 from wx.lib.wordwrap import wordwrap
@@ -108,13 +110,26 @@ class LeftPanel(scrolled.ScrolledPanel):
         notecardText="Notecard Text Appears Here"
         #use test to loop through and add the notecards dynamically
         # show the bitmap, (5, 5) are upper left corner coordinates
-        note=wx.StaticBitmap(self, -1, bmp, (0, 140))
+
+        #self.note=wx.StaticBitmap(self, -1, bmp, (0, 140))
+
+        #self.text = wx.StaticText(self, -1, "", (self.width, self.height), style=(wx.ALIGN_CENTRE_HORIZONTAL| wx.TE_MULTILINE ))
+        #self.text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        #self.text.SetSize(self.text.GetBestSize())
+
+        #font =  dc.GetFont()
+        #font.SetPointSize(15)
+        #font.SetWeight(wx.FONTWEIGHT_BOLD)
         displaySize=wx.DisplaySize()
-        text = wx.StaticText(self, -1, notecardText,(0, 140))
-        text.SetFont(wx.Font(5, wx.SWISS, wx.NORMAL, wx.BOLD))
-        text.SetSize(text.GetBestSize())
-        sizer.Add(text,0,wx.EXPAND)
-        sizer.Add(note, 0, wx.EXPAND)
+
+        #next three lines work for a static bitmap
+        #text1 = wx.StaticText(self.note, -1, notecardText, style=(wx.ALIGN_CENTRE_HORIZONTAL))
+        #text1.SetFont(wx.Font(5, wx.SWISS, wx.NORMAL, wx.BOLD))
+        #text1.SetSize(text1.GetBestSize())
+        ###
+
+        #sizer.Add(text1,0,wx.EXPAND)
+        '''sizer.Add(self.note, 0, wx.EXPAND)
         sizer.AddSpacer(10)
         test=wx.StaticBitmap(self, -1, bmp, (0, 140))
         sizer.Add(test, 0, wx.EXPAND)
@@ -126,7 +141,26 @@ class LeftPanel(scrolled.ScrolledPanel):
         sizer.Add(note3, 0, wx.EXPAND)
         sizer.AddSpacer(10)
         note4=wx.StaticBitmap(self, -1, bmp, (0, 140))
-        sizer.Add(note4, 0, wx.EXPAND)
+        sizer.Add(note4, 0, wx.EXPAND)'''
+        
+        #button testing
+        image2 = wx.Image(imageFile, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.testButton = wx.BitmapButton(self, id=-1, bitmap=image2,
+         size=(image2.GetWidth()+5, image2.GetHeight()+5), style=wx.BU_AUTODRAW)
+        text = wx.StaticText(self.testButton, -1, notecardText, style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.TE_MULTILINE))
+        text.SetFont(wx.Font(5, wx.SWISS, wx.NORMAL, wx.BOLD))
+        text.SetSize(text.GetBestSize())
+        sizer.Add(text,0,wx.EXPAND)
+        sizer.Add(self.testButton,0,wx.EXPAND)
+        sizer.AddSpacer(10)
+        
+        self.testButton1 = wx.BitmapButton(self, id=-1, bitmap=image2,
+         size=(image2.GetWidth()+5, image2.GetHeight()+5), style=wx.BU_AUTODRAW)
+        text1 = wx.StaticText(self, -1, notecardText, style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.TE_MULTILINE))
+        text1.SetFont(wx.Font(5, wx.SWISS, wx.NORMAL, wx.BOLD))
+        text1.SetSize(text1.GetBestSize())
+        sizer.Add(text1,0,wx.EXPAND)
+        sizer.Add(self.testButton1,0,wx.EXPAND)
         self.SetSizer(sizer)
         self.SetAutoLayout(1)
         self.SetupScrolling()
@@ -160,6 +194,7 @@ class RightPanel(wx.Panel):
         nextImage = "images\\buttons\\NextButtonNew.png"
         previousImage="images\\buttons\\BackButtonNew.png"
         flipImage="images\\buttons\\FlipButton1.png"
+        shuffleImage="images\\buttons\\ShuffleButton1.png"
         #Need to loop if at start of list or end of list
 
         #Next Button Init
@@ -184,6 +219,13 @@ class RightPanel(wx.Panel):
         pos=(970, 520), size=(image2.GetWidth()+5, image2.GetHeight()+5), style=wx.BU_AUTODRAW)
         self.flipButton.Bind(wx.EVT_BUTTON, lambda event: self.flipButtonClick(event, cards))
         self.flipButton.SetToolTip(wx.ToolTip("Go to Front/Back of Flashcard"))
+
+        #Shuffle Button Init
+        image4 = wx.Image(shuffleImage, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.shuffleButton = wx.BitmapButton(self, id=-1, bitmap=image4,
+        pos=(10, 520), size=(image2.GetWidth()+5, image2.GetHeight()+5), style=wx.BU_AUTODRAW)
+        self.shuffleButton.Bind(wx.EVT_BUTTON, lambda event: self.shuffleButtonClick(event, cards))
+        self.shuffleButton.SetToolTip(wx.ToolTip("Shuffle the Deck of Cards"))
 
         #self.nextButton = wx.Button(self,label='Next', pos=(990,305), size=(80,50))
         #Binds the trigger event fror going to previous flashcard
@@ -247,10 +289,15 @@ class RightPanel(wx.Panel):
 
 
 
-    #next button click event
+    #fliip button click event
     def flipButtonClick(self,event, cards):
         cards.flip()
         #self.updateText.Label = cards.getInfo()
+        self.updateText(cards)
+
+    #shuffle button click event
+    def shuffleButtonClick(self,event,cards):
+        #need to add shuffle here
         self.updateText(cards)
 
 
